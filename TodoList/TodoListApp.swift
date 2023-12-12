200935270fbdd02a994d0f20ae6c0e7613ebd8b4
Truong Xuan Nguyen <truong.xuan.nguyen@advesa.com>
2023-12-12 15:26:16 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by MacintoshHD on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
